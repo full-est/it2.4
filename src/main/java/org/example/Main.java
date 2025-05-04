6126dec17272ccbf5d5778350becf363a280b269
full-est <xamilion48@gmail.com>
2025-05-04 19:42:20 +0500

    di с помощью xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,15 @@
 package org.example;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Car car = context.getBean("carBean", Car.class);
-        Device keyboard = context.getBean("Keyboard", Keyboard.class);
-        Device headphones = context.getBean("Headphones", Headphones.class);
-        Device graphicTablet = context.getBean("GraphicTablet", GraphicTablet.class);
+        Shop shop = context.getBean("shop", Shop.class);
 
-        Dealer dealer = new Dealer(car);
-        Shop shop = new Shop(keyboard);
-        Shop shop2 = new Shop(headphones);
-        Shop shop3 = new Shop(graphicTablet);
         shop.printInfo();
-        shop2.printInfo();
-        shop3.printInfo();
-        dealer.printModel();
         context.close();
     }
 }
--- src/main/java/org/example/SpringConfig.java
+++ src/main/java/org/example/SpringConfig.java
@@ -1,28 +1,28 @@
-package org.example;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SpringConfig {
-    @Bean(name = "carBean")
-    public Mercedes getBean() {
-        return new Mercedes();
-    }
-
-    @Bean(name = "Keyboard")
-    public Keyboard keyboard(){
-        return new Keyboard();
-    }
-
-    @Bean(name = "Headphones")
-    public Headphones headphones(){
-        return new Headphones();
-    }
-
-    @Bean(name = "GraphicTablet")
-    public GraphicTablet graphicTablet(){
-        return new GraphicTablet();
-    }
-}
+//package org.example;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SpringConfig {
+//    @Bean(name = "carBean")
+//    public Mercedes getBean() {
+//        return new Mercedes();
+//    }
+//
+//    @Bean(name = "Keyboard")
+//    public Keyboard keyboard(){
+//        return new Keyboard();
+//    }
+//
+//    @Bean(name = "Headphones")
+//    public Headphones headphones(){
+//        return new Headphones();
+//    }
+//
+//    @Bean(name = "GraphicTablet")
+//    public GraphicTablet graphicTablet(){
+//        return new GraphicTablet();
+//    }
+//}
 
